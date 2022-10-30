1a59a601cea4b2c8a0d2dcfbefe886db16d2df43
Moe Martinez <deadfocus@gmail.com>
2022-10-29 21:52:33 -0500

    waits for cam permissions to join and added IDs to video

--- public/js/script.js
+++ public/js/script.js
@@ -13,69 +13,90 @@ if(form){
     });
     }
 
+//using https://medium.com/codesphere-cloud/building-a-video-chat-app-with-socket-io-peerjs-codesphere-b0663bcbe3d7
 /* chat room */
 const videoContainer = document.querySelector(".videoContainer");
-const myCamera = document.createElement("video");
-myCamera.muted = true;
+if(videoContainer){
 
-/* socket stuff for chat */
+    const myCamera = document.createElement("video");
+    myCamera.muted = true;
+    
+    /* socket stuff for chat */
+    
 
-const socket = io("/");
-let peer = new Peer(undefined, {
-    path: "/peerjs",
-    host: "/",
-    port: "8000"
-});
-
-const addVideoStream = (video, stream)=>{
-
-    console.log(stream)
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", ()=>{
-        video.play();
-    })
-
-    videoContainer.append(video);
-
-};
-
-navigator.mediaDevices.getUserMedia({audio: true, video: true})
-.then((stream)=>{
-
-    addVideoStream(myCamera, stream);
-
-    peer.on("call", (call)=>{
-        call.answer(stream);
-        const video = document.createElement("video");
+    
+    navigator.mediaDevices.getUserMedia({audio: true, video: true})
+    .then((stream)=>{
+        const socket = io("/");
+        let peer = new Peer();
+        
+        const addVideoStream = (video, stream)=>{
+            
+            video.srcObject = stream;
+            video.addEventListener("loadedmetadata", ()=>{
+                video.play();
+            })
+            
+            videoContainer.append(video);
+            
+        };
+        
+        addVideoStream(myCamera, stream); //this adds our video;
+        
+        peer.on("call", (call)=>{
+            call.answer(stream);
+            const video = document.createElement("video");
             call.on("stream", (userVideoStream)=>{
-                addVideoStream(video, userVideoStream)
+                addVideoStream(video, userVideoStream);
             });
-    });
+        });
     socket.on("user-connected", (userID)=>{
-        console.log(userID)
+        console.log(`${userID} has connected`)
         connectToNewUser(userID, stream);
     });
+    
+    socket.on("user-disconnected",(userID)=>{
+        const removeVideo = document.querySelector(`[data-user-id="${userID}"]`);
+        if(removeVideo){
+            removeVideo.parentNode.removeChild(removeVideo)
+        }
+    });
+
+
+    peer.on("open", (id)=>{
+        myCamera.setAttribute("data-user-id", id )
+        socket.emit("join-chat", ROOM_ID, id);
+    })
+
+
+    const connectToNewUser = (userID, stream)=>{
+        const call = peer.call(userID, stream);
+        const video = document.createElement("video");
+        call.on("stream", (userVideoStream)=>{
+            addVideoStream(video, userVideoStream)
+            video.setAttribute("data-user-id", userID )
+        });
+        
+        call.on("close", ()=>{
+            const removeVideo = document.querySelector(`[data-user-id="${userID}"]`);
+            if(removeVideo){
+                removeVideo.parentNode.removeChild(removeVideo)
+            }
+        })
+        
+    };
+
+
+    window.addEventListener('beforeunload', (event) => { socket.close() });
+
 });
 
 
-peer.on("open", (id)=>{
-    socket.emit("join-chat", ROOM_ID, id);
-})
 
 
-const connectToNewUser = (userID, stream)=>{
-    const call = peer.call(userID, stream);
-    const video = document.createElement("video");
-    call.on("stream", (userVideoStream)=>{
-        addVideoStream(video, userVideoStream)
-    });
 
-    call.on('close', () => {
-        video.remove()
-    })
-};
-window.addEventListener('beforeunload', (event) => { socket.close() });
 };
 
+}
 
 
